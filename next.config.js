6b6f0d779a3cc9d05b64e199e3a1c6a18adb7a52
Monica Laura Burns <monicalauraburns@yahoo.com>
2024-03-21 15:21:02 +0200

    Prepared the app for live deployment on Vercel

--- next.config.js
+++ next.config.js
@@ -1,8 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['d13k13wj6adfdf.cloudfront.net'], 
+    domains: ["d13k13wj6adfdf.cloudfront.net"],
   },
-  
 };
-module.exports = nextConfig
+
+module.exports = {
+  ...nextConfig,
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // Allow all origins
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
+};
